e7eec9ddc5afa64425335f7346bc962e98c57939
Hannah Drury <hannah_drury@yahoo.com>
2019-12-07 22:45:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function happyHolidayTo(holiday, name)
 
 function holidayCountDown(holiday, days)
 {
-  return `It\'s ${days} days until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`
 }
